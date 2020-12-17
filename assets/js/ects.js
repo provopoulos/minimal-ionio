9c8ed1b4542a16ebf5cc8752939aaa23eea8176c
andreaspappoutas <44147982+andreaspappoutas@users.noreply.github.com>
2020-12-18 01:52:27 +0200

    Update ects.js

--- assets/js/ects.js
+++ assets/js/ects.js
@@ -61,7 +61,7 @@ function ects_total_TEMP(m){//function gia xrisi tou autoCheck
 }
 
 function Create_Arrays(semester,type){
-  var array
+  var array;
   '{% assign posts = site.collections | where: 'label', include.l | first %}'
   '{% assign pages = posts.docs | sort: "title" %}'
   '{% for post in pages %}'
